da70b6333958d22c7ad74b9b24bf8b6610b18539
khaledalkhayrat <vrt38694@gmail.com>
2025-05-01 20:15:43 +0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5050',
+        target: 'https://backend-yyv2.onrender.com',
         changeOrigin: true,
         secure: false,
       },
